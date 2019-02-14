ef13381dbd511cd94f95a790feb5db6a797e33ea
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-02-14 08:54:59 +0300

    Create notifySlackFailureBuild.groovy

---
+++ vars/notifySlackFailureBuild.groovy
@@ -0,0 +1,6 @@
+def call() {
+    def now = new Date().format("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone('UTC'))
+    def msg = "*[${now}]*  `${env.JOB_NAME}` Pipeline executed `FAILED` ${env.BUILD_URL}"
+    slackSend baseUrl: env.SLACK_URL, channel: env.SLACK_CHANNEL, message: msg, color: '#FF9FA1',
+        token: env.SLACK_TOKEN
+}
