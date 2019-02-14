489935541f26310c47b104bf08b7c0a1f173372c
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-02-14 09:02:08 +0300

    Update checkLog.groovy

--- vars/checkLog.groovy
+++ vars/checkLog.groovy
@@ -1,14 +1,22 @@
 def call(String stage) {
     step([$class: 'LogParserPublisher', failBuildOnError: true, parsingRulesPath: '/home/ubuntu/log-parser-rule', useProjectRule: false])
-    if ('FAILURE' == currentBuild.result) {
+    if ('FAILURE' == currentBuild.result || 'ABORTED' == currentBuild.result) {
         notifySlackFailureStage(env.STAGE_NAME)
         throw error("")
+    } else 
+    {
+        notifySlackSuccessStage(env.STAGE_NAME)
     }
 }
-
 def notifySlackFailureStage(String stage) {
     def now = new Date().format("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone('UTC'))
     def msg = "*[${now}]*  `${env.JOB_NAME}` ${stage} `FAILED`"
     slackSend baseUrl: env.SLACK_URL, channel: env.SLACK_CHANNEL, message: msg, color: '#FF9FA1',
+        token: env.SLACK_TOKEN
+}
+def notifySlackSuccessStage(String stage {
+    def now = new Date().format("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone('UTC'))
+    def msg = "*[${now}]*  `${env.JOB_NAME}` ${stage} `SUCCESS`"
+    slackSend baseUrl: env.SLACK_URL, channel: env.SLACK_CHANNEL, message: msg, color: '#BDFFC3',
         token: env.SLACK_TOKEN
 }
