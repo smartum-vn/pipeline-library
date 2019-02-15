ecbdb55cce997d8bc41e3d03fc014ed7f0d43e59
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-02-15 10:50:30 +0300

    Update getDependentPipelinesStatus.groovy

--- vars/getDependentPipelinesStatus.groovy
+++ vars/getDependentPipelinesStatus.groovy
@@ -1,7 +1,7 @@
 def call(String[] pipelines) {
     def stagingBuildStatus = 'SUCCESS'
     Jenkins.instance.getAllItems(Job.class).each {it->
-        if (projectList.contains(it.name)) {
+        if (pipelines.contains(it.name)) {
             def lastBuild = it.getLastBuild()
             if (lastBuild == null || (lastBuild != null && lastBuild.getResult().toString() != 'SUCCESS')) {
                 stagingBuildStatus = 'FAILURE'
