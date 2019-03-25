e21ac455919e155d739b46d5b3d35eae1e3e2be1
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-03-25 10:04:16 +0300

    Update notifySlackStart.groovy

--- vars/notifySlackStart.groovy
+++ vars/notifySlackStart.groovy
@@ -1,4 +1,4 @@
 def call() {
-    def msg = "*[${now}]*  `${env.JOB_NAME}` STARTED ` DO NOT TOUCH "
+    def msg = "`${env.JOB_NAME}` STARTED `DO NOT TOUCH`"
     slackSend baseUrl: env.SLACK_URL, channel: 'backend-data_process' , message: msg, color: '#D4DADF', token: 'k3xXZqkEyYfROa7YR92pK9ig'
 }
