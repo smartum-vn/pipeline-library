eb219970ac7b52046e5454cffd7d1322d1d9ff98
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-03-25 09:36:52 +0300

    Update notifySlackStart.groovy

--- vars/notifySlackStart.groovy
+++ vars/notifySlackStart.groovy
@@ -1,1 +1,6 @@
-
+def call() {
+    def now = new Date().format("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone('UTC'))
+    def msg = "*[${now}]*  `${env.JOB_NAME}` ${env.STAGE_NAME} `STARTED`"
+    slackSend baseUrl: env.SLACK_URL, channel: 'backend_data-process', message: msg, color: '#D4DADF',
+        token: 'KTt42sxU8xU1jTF3Ix3pwI13'
+}
