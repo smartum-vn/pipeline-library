811f665f0ee2f6aba4beb60d50d2759c85af033a
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-03-25 18:10:21 +0300

    Update scaleDown.groovy

--- vars/scaleDown.groovy
+++ vars/scaleDown.groovy
@@ -1,8 +1,6 @@
 def call(String instanceId, String type, boolean stopAfterScaleDown) {
 
     STATE = sh(returnStdout: true, script: "echo \$(aws ec2 describe-instances --instance-ids $instanceId --output text --query Reservations[*].Instances[*].State.Name)")
-    TYPE = TYPE.replaceAll("\\s", "")
-
         if (STATE == "running") {
             sh "aws ec2 stop-instances --instance-ids $instanceId"
             sh "aws ec2 wait instance-stopped --instance-ids $instanceId"
