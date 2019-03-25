4fa4f1455c5557167b4136d67a7189a787fbf078
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-03-25 10:48:37 +0300

    Update checkAndScaleUp.groovy

--- vars/checkAndScaleUp.groovy
+++ vars/checkAndScaleUp.groovy
@@ -1,10 +1,14 @@
-def call() {
-    sh 'aws ec2 describe-instances --instance-ids i-0ca541748d138bbe4 --output text --query Reservations[*].Instances[*].State.Name'
-    sh 'aws ec2 stop-instances --instance-ids i-0ca541748d138bbe4'
-    sh 'aws ec2 wait instance-stopped --instance-ids i-0ca541748d138bbe4'
-    sh 'aws ec2 modify-instance-attribute --instance-id i-0ca541748d138bbe4 --instance-type t2.medium'
-
-    sh 'aws ec2 start-instances --instance-ids i-0ca541748d138bbe4'
-    sh 'aws ec2 wait instance-running --instance-ids i-0ca541748d138bbe4'
-    sh 'aws ec2 describe-instances --instance-ids i-0ca541748d138bbe4 --output text --query Reservations[*].Instances[*].State.Name'
+def call(String instanceId, String type) {
+    
+    STATE = sh(returnStdout: true, script: "echo \$(aws ec2 describe-instances --instance-ids $instanceId --output text --query Reservations[*].Instances[*].State.Name)")
+    TYPE = sh(returnStdout: true, script: "echo \$(aws ec2 describe-instances --instance-ids $instanceId --output text --query Reservations[*].Instances[*].InstanceType)")
+    if (TYPE != type) {
+        if (STATE == 'running') {
+            sh "aws ec2 stop-instances --instance-ids $instanceId"
+            sh "aws ec2 wait instance-stopped --instance-ids $instanceId"
+        }
+        sh "aws ec2 modify-instance-attribute --instance-id $instanceId --instance-type $type"
+        sh "aws ec2 start-instances --instance-ids $instanceId"
+        sh "aws ec2 wait instance-running --instance-ids $instanceId"
+    }
 }
