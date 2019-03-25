204bd5528ce6af9a9084e967b77248b668e4ea68
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-03-25 09:59:25 +0300

    Delete notifySlackStart.groovy

--- vars/notifySlackStart.groovy
+++
@@ -1,6 +0,0 @@
-def call() {
-    def now = new Date().format("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone('UTC'))
-    def msg = "*[${now}]*  `${env.JOB_NAME}` ${env.STAGE_NAME} `STARTED`"
-    slackSend baseUrl: env.SLACK_URL, channel: 'backend_data-process', message: msg, color: '#D4DADF',
-        token: 'ATA1vsRzCd7fL3T8AsBceh9X'
-}
