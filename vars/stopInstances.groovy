00f4fade36c16890713d021b2c9f0c221a675712
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-03-25 18:07:39 +0300

    Update stopInstances.groovy

--- vars/stopInstances.groovy
+++ vars/stopInstances.groovy
@@ -1,7 +1,7 @@
 def call(String[] instances) {
     for (int i = 0; i < instances.size(); i++) {
         def instance = instances[i]
-        def state = sh(returnStdout: true, script: "echo \$(aws ec2 describe-instances --instance-ids $instanceId --output text --query Reservations[*].Instances[*].State.Name)")
+        def state = sh(returnStdout: true, script: "echo \$(aws ec2 describe-instances --instance-ids $instance --output text --query Reservations[*].Instances[*].State.Name)")
         state = state.replaceAll("\\s", "")
         if (state != 'stopped') {
             sh "aws ec2 stop-instances --instance-ids $instance"
