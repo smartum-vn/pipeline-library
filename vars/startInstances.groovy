514a3f60a03bc9030268d611be94fd7ea7a381ad
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-03-25 18:01:50 +0300

    Update startInstances.groovy

--- vars/startInstances.groovy
+++ vars/startInstances.groovy
@@ -1,5 +1,5 @@
 def call(String[] instances) {
-    for (int i = 0; i < arr.size(); i++) {
+    for (int i = 0; i < instances.size(); i++) {
         def instance = arr[i]
         def state = sh(returnStdout: true, script: "echo \$(aws ec2 describe-instances --instance-ids $instanceId --output text --query Reservations[*].Instances[*].State.Name)")
         state = state.replaceAll("\\s", "")
