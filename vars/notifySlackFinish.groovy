0a8cdd86aba48936de495c9e08741d2337e7e71e
Vladimir N <42865574+smartum-vn@users.noreply.github.com>
2019-03-26 14:26:41 +0300

    Update notifySlackFinish.groovy

--- vars/notifySlackFinish.groovy
+++ vars/notifySlackFinish.groovy
@@ -1,5 +1,5 @@
 def call() {
     def msg = "`${env.JOB_NAME}` FINISHED"
-    slackSend baseUrl: env.SLACK_URL, channel:'backend-data-process' , message: msg, color: '#D4DADF',
+    slackSend baseUrl: env.SLACK_URL, channel:'backend_data-process' , message: msg, color: '#D4DADF',
         token: 'k3xXZqkEyYfROa7YR92pK9ig'
 }
